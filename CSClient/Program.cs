37e0743ef48e7bb2709bcb7ab9bd65f545fda503
zisi.mo <zisi.mo@miHoYo.com>
2019-04-23 17:25:11 +0800

    Echo Demo

---
+++ CSClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CSClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var sendSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sendSock.Connect("127.0.0.1", 8888);
+            Console.WriteLine("[Client] Connect To Server");
+
+            var sendStr = "OMG";
+            var sendBuff = Encoding.Default.GetBytes(sendStr);
+            sendSock.Send(sendBuff);
+            Console.WriteLine("[Client] Send To Server: " + sendStr);
+
+            var receiveBuff = new byte[1024];
+            var readCount = sendSock.Receive(receiveBuff);
+            var receiveStr = Encoding.Default.GetString(receiveBuff, 0, readCount);
+            Console.WriteLine("[Client] Receive from Server: " + receiveStr);
+
+            sendSock.Close();
+            Console.WriteLine("[Client] Close!");
+
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ CSServer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CSServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //init bind
+            var listenSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var ipAddr = IPAddress.Parse("127.0.0.1");
+            var ipEndPoint = new IPEndPoint(ipAddr, 8888);
+            listenSock.Bind(ipEndPoint);
+
+            //listen
+            listenSock.Listen(0);
+            Console.WriteLine("[Server] Start Listen");
+
+            while (true)
+            {
+                //accept
+                var connectSock = listenSock.Accept();
+                Console.WriteLine("[Server] Accept");
+
+                //receive
+                var readBuff = new byte[1024];
+                var readCount = connectSock.Receive(readBuff);
+                var readStr = Encoding.Default.GetString(readBuff, 0, readCount);
+                Console.WriteLine("[Server] Recevive " + readStr);
+
+                //send
+                var sendBuff = Encoding.Default.GetBytes(readStr);
+                connectSock.Send(sendBuff);
+            }
+        }
+    }
+}
+ 
